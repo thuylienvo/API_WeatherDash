76223690b2cbdfa5bb86485199a3b775b15b1779
Mimi Vo <mimzy414@gmail.com>
2021-08-24 17:47:11 -0500

    added search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,20 @@ var searchCity = '';
 var searchCityInput = document.querySelector('#selectCity');
 var currentWeather = document.querySelector('#current-forecast');
 
-
-
-
 // Moment
-var todaysDate = moment().format('dddd, MMMM Do');
+var todaysDate = moment().format('(MM/DD/YYYY');
 
+
+// Search history 
+function weatherHistory(city) {
+  let btn = 
+  $('<button type="button" class="btn btn-secondary my-2" id="save1">' + city + '</button>');
+
+  if ($("searchHistory").children().length > 6) {
+    $("#searchHistory").find(":first-child").remove();
+    btn.appendTo("#searchHistory"); 
+  } else {
+    btn.appendTo("searchHistory");
+  }
+}
 
