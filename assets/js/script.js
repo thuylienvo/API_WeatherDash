1f16c1bd65bf8659f8f54fc73af40fb61ba623f4
Mimi Vo <mimzy414@gmail.com>
2021-08-24 17:18:44 -0500

    added a few variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    console.log("function was called");
-  };
-  
-  getUserRepos();
+var historyArray = new Array(6).fill('');
+var historyList  = $('#searchHistory');
+var searchCity = '';
+var searchCityInput = document.querySelector('#selectCity');
+var currentWeather = document.querySelector('#current-forecast');
+
+
+
+
+// Moment
+var todaysDate = moment().format('dddd, MMMM Do');
+
+
